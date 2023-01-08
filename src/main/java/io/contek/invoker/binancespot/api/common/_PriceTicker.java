feda94fcc93f630ea318a99722b417950b15c2a3
kong_k <florin.manolache@gmail.com>
2023-01-08 17:08:52 +0200

    GetTickerPrice impl

---
+++ src/main/java/io/contek/invoker/binancespot/api/common/_PriceTicker.java
@@ -0,0 +1,11 @@
+package io.contek.invoker.binancespot.api.common;
+
+import javax.annotation.concurrent.NotThreadSafe;
+import java.math.BigDecimal;
+
+@NotThreadSafe
+public class _PriceTicker {
+
+  public String symbol;
+  public BigDecimal price;
+}
