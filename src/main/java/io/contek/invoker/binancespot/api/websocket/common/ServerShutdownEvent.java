a172955ae265af648912d2043877edc1f0af3699
kong <kong_k@hotmail.com>
2026-07-19 20:52:37 +0300

    ServerShutdownEvent

---
+++ src/main/java/io/contek/invoker/binancespot/api/websocket/common/ServerShutdownEvent.java
@@ -0,0 +1,6 @@
+package io.contek.invoker.binancespot.api.websocket.common;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+@NotThreadSafe
+public final class ServerShutdownEvent extends WebSocketEventData {}
